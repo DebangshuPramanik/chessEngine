a9df2f64e31961be401bd7880bc3fc4c76561638
kirawano <nuhuh2964@gmail.com>
2024-03-28 11:10:29 -0700

    Fixed bug where double pawn moves wouldn't work due to find_best_move not creating a copy of the board

--- src/bot.py
+++ src/bot.py
@@ -10,8 +10,8 @@ class Bot:
         self.game = game
 
     # creates a copy of the board, makes a move on that copy and evaluates the board too
-    def evaluate_move(self, move, piece, castling=False):
-        temp_board = copy.deepcopy(self.game.board)
+    def evaluate_move(self, board, move, piece, castling=False):
+        temp_board = copy.deepcopy(board)
         temp_board.move(piece, move, testing=True, castling=castling)
 
         return temp_board.evaluate_board()
@@ -24,16 +24,17 @@ class Bot:
         #   implement recursion to a specific depth (and change the default depth while you're at it to 4)
         #   Remember that depth must be an even number if you're playing as white, and an odd number if black
 
+        temp_board = copy.deepcopy(board)
 
         # Find a better solution later
         max = -999999999999999999999999999
         for row in range(ROWS):
             for col in range(COLS):
-                if board.squares[row][col].has_team_piece("white"):
-                    moves = board.calc_moves(
-                        board.squares[row][col].piece, row, col)
-                    for move in board.squares[row][col].piece.moves:
-                        if self.evaluate_move(move, board.squares[row][col].piece) > max:
-                            max = self.evaluate_move(move, board.squares[row][col].piece)
+                if temp_board.squares[row][col].has_team_piece("white"):
+                    moves = temp_board.calc_moves(
+                        temp_board.squares[row][col].piece, row, col)
+                    for move in temp_board.squares[row][col].piece.moves:
+                        if self.evaluate_move(temp_board, move, temp_board.squares[row][col].piece) > max:
+                            max = self.evaluate_move(board, move, temp_board.squares[row][col].piece)
 
         return max
