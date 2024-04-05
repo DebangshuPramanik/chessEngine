39dbd8d8e07283ceca69b333d9b98d769542cfcc
kirawano <coledt54@gmail.com>
2024-04-05 16:21:21 -0700

    fixed error with bot.py, still not fixed.

--- src/bot.py
+++ src/bot.py
@@ -8,6 +8,8 @@ class Bot:
     def __init__(self, game, player):
         self.game = game
         self.player = player
+        self.best_piece = None
+        self.best_move = None
 
     # creates a copy of the board, makes a move on that copy and evaluates the board too
     def evaluate_move(self, board, move, piece, castling=False):
@@ -17,37 +19,30 @@ class Bot:
         return temp_board.evaluate_board()
 
     # loops through the board and evaluates each possible move given a depth
-    def find_best_move(self, board, depth=2):
+    def mini_max(self, board, depth):
         # TODO: implement recursion to find the best move
-        score=0
-        if depth == 0:
-            return board.evaluate_board()
-
         player_shorthand = {0: "white", 1: "black"}
-        player_mod = 1 if self.game.board.counter % 2 == 0 else -1
+        player_mod = 1 if board.counter % 2 == 0 else -1
+
+        if depth == 0:
+            return player_mod*board.evaluate_board()
 
         temp_board = copy.deepcopy(board)
-        best_move = None
-        best_piece = None
 
-        # Find a better solution later
-        max = -999999999999999999999999999
-        for row in range(ROWS):
-            for col in range(COLS):
-                if temp_board.squares[row][col].has_team_piece(
-                    player_shorthand[(self.game.board.counter % 2)]
-                ):
-                    temp_board.calc_moves(temp_board.squares[row][col].piece, row, col)
-                    for move in temp_board.squares[row][col].piece.moves:
-                        temp_board.move(piece
-                        score = self.find_best_move(temp_board, depth=(depth-1))
-                        #m,p = evaluated_move
+        maxi = -999999999999999999999999999
+        for tup in temp_board.calc_color_moves(player_shorthand[temp_board.counter % 2]): 
+            piece,move = tup
+            temp_board.move(piece, move, testing=True)
+            score = self.mini_max(temp_board, depth=(depth-1))
 
-                        #score = evalute_move(temp_board, m, p)
+            if player_mod * score > maxi:
+                maxi = player_mod * score
+                self.best_move = move
+                self.best_piece = piece
 
-                        if player_mod * score > max:
-                            max = player_mod * score
-                            best_move = m
-                            best_piece = p
+        return maxi
 
-        return evaluated_move
+    def find_best_move(self, board, depth=2):
+        a = self.mini_max(board, depth)
+        print(a)
+        return (self.best_move, self.best_piece)
