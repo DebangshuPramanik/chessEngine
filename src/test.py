34228041564d44dbb7e845500f29328233e1d582
sam <samcormode@gmail.com>
2024-04-04 22:55:00 -0700

    test update for more testing

--- src/test.py
+++ src/test.py
@@ -13,20 +13,23 @@ print(nb.calc_moves_no_check((1, 7)))
 
 # nb.print()
 #
-#nb.from_string(
-#    """
-#p . k p p p p p
-#. P p p p p p p
-#. . . . . . . .
-#. . . . . . . .
-#. . . . . . . .
-#. . . . . . . .
-#P P P P P P P P
-#P P P P P P P P
-#"""
-#)
+nb.from_string(
+"""
+r . . q . r k .
+. . p b . p B p
+p . n . . n . .
+. p b . N . . .
+. . B p P . . .
+. . . P . . R .
+P P P . . P P P
+R N . Q . . K .
+"""
+)
 #
-#nb.print()
+nb.print(True)
+nb.move(Move.from_string('g7->f6'))
+nb.print(True)
+print(nb.calc_moves((0, 3)))
 #
 #print([str(m) for m in nb.calc_moves((0, 0))])
 #print([str(m) for m in nb.calc_moves((1, 1))])
@@ -35,17 +38,20 @@ print(nb.calc_moves_no_check((1, 7)))
 # print(nb.in_check(Move()))
 
 
-print("""
-The format for a move is like: a4->b6
-To signify a castle, do the move the king would make.
-To signify promotion, add an =Piece, like a7->a8=Q.
-Valid piece values are [N,B,R,Q] (must be uppercase)
-There is no checking for valid moves. (but promotion will only work if going to the last row)
-""")
 
-print(Move.from_string("a7->a8=Q"))
+def interactive_test():
+    print("""
+    The format for a move is like: a4->b6
+    To signify a castle, do the move the king would make.
+    To signify promotion, add an =Piece, like a7->a8=Q.
+    Valid piece values are [N,B,R,Q] (must be uppercase)
+    There is no checking for valid moves. (but promotion will only work if going to the last row)
+    """)
 
-while True:
-    nb.print(True)
-    m = Move.from_string(input("Enter move:").rstrip())
-    nb.move(m)
+    print(Move.from_string("a7->a8=Q"))
+    nb = NumberBoard(Board())
+
+    while True:
+        nb.print(True)
+        m = Move.from_string(input("Enter move:").rstrip())
+        nb.move(m)
